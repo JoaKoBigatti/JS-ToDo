3e75f8f0aa819390908132190a888e9d014b4453
joakobigatti <imwinkelriedjoaquin@gmail.com>
2023-04-26 18:19:51 -0300

    v0.0.6

--- src/todos/app.js
+++ src/todos/app.js
@@ -1,16 +1,30 @@
+import todoStore from '../store/todo.store';
 import html from './app.html?raw'
+import { renderTodos } from './use-cases';
+
+const ElementIDs = {
+    TodoList: '.todo-list',
+}
+
 /**
  * 
- * @param {string} elementID 
+ * @param {string} elementId 
  */
 
 export const App = (elementId)=>{
+
+    const displayTodos=()=>{
+        const todos=todoStore.getTodos(todoStore.getCurrentFilter());
+        renderTodos(ElementIDs.TodoList,todos);
+    }
+
+
     //cuando la funcion app se llama
     (()=>{
         const app= document.createElement('div');
         app.innerHTML=html;
         document.querySelector(elementId).append(app);
-
+        displayTodos();
     })();
 
 
